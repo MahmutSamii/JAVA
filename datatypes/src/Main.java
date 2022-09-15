5abc337741656c5f0f597c82c109fc682d9d99fb
MahmutSamii <samimahmut34@gmail.com>
2022-09-15 18:09:31 +0300

    datatypes

---
+++ datatypes/src/Main.java
@@ -0,0 +1,12 @@
+public class  Main {
+    public static void main(String[] args) {
+        boolean bool = true;
+        char sayi1 = 1;
+        byte sayi2 = -128;
+        short sayi3 = -32768;
+        int sayi4 = -2147483647;
+        long sayi5 = 999999999;
+        // float sayi6 = 3.23;
+        double sayi6 = 3.23;
+    }
+}
