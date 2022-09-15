782945610f4706802b8bd9ac12157cd0aa8d395b
MahmutSamii <samimahmut34@gmail.com>
2022-09-15 19:05:24 +0300

    conditions

---
+++ conditions/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 20;
+
+        if (sayi< 20){
+            System.out.println("sayi yirmiden küçüktür");
+        } else if (sayi==20) {
+            System.out.println("sayi yirmiye eşittir");
+        } else{
+            System.out.println("sayi yirmiden küçük değildir");
+        }
+    }
+}
